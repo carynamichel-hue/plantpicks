2bab822bbff6894782e3d9ca91ef3c849f8c7486
carynamichel-hue <caryn.amichel@gmail.com>
2026-08-25 12:16:24 -0400

    deploy 2026-08-25 16:16

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
  * never change within an event; re-fetching them on a bad connection is the
  * one thing guaranteed to ruin this.
  */
-var CACHE = 'plantpicks-202608251604';
+var CACHE = 'plantpicks-202608251616';
 var SHELL = ['./', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', function (e) {
